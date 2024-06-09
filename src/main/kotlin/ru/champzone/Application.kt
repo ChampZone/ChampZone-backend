eca23e3d7930a5e6d8e893de6ebb352e90e924a4
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-06-09 16:33:30 +0300

    initial commit

---
+++ src/main/kotlin/ru/champzone/Application.kt
@@ -0,0 +1,13 @@
+package ru.champzone
+
+import io.ktor.server.application.*
+import ru.champzone.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/ru/champzone/plugins/Serialization.kt
@@ -0,0 +1,9 @@
+package ru.champzone.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+    }
+}
